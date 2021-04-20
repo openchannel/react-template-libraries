b0a2cec7da4545f6cfba489746555e98ba5c0d11
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-04-20 16:52:57 +0300

    [RT-7] upd: config for the storybook and for the rollup

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,6 +11,16 @@ module.exports = {
       ],
       include: path.resolve(__dirname, '../'),
     });
+
+    const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test('.svg'));
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: 'pre',
+      loader: require.resolve('@svgr/webpack'),
+    });
+
     return config;
   },
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
--- tsdx.config.js
+++ tsdx.config.js
@@ -1,11 +1,13 @@
 const styles = require('rollup-plugin-styles');
-const rebasePlugin = require('rollup-plugin-rebase');
+const rebasePlugin = require('rollup-plugin-rebase')
+const inlineSvg = require('rollup-plugin-inline-svg');
 
 module.exports = {
 	rollup(config, options) {
 		config.plugins.push(
+			inlineSvg(),
 			styles(),
-			rebasePlugin()
+			rebasePlugin(),
 		);
 		return config;
 	},
