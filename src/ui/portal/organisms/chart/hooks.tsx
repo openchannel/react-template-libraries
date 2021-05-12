be6b4762bcb397cf10618a2eee774f8bcc9d94ca
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-05-12 17:30:51 +0300

    [RT-24] refactor: add util fns with constants. moved types, chart-state to own files.

--- src/ui/portal/organisms/chart/hooks.tsx
+++ src/ui/portal/organisms/chart/hooks.tsx
@@ -1,23 +1,24 @@
 import * as React from 'react';
 
-import { chartReducer, chartInitialState, SET_TABLE_DATA, UPDATE_SORT } from './utils';
+import { chartReducer, chartInitialState, SET_TABLE_DATA, UPDATE_SORT } from './chart-state';
 
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const useChartReducer = () => {
 	const [state, dispatch] = React.useReducer(chartReducer, chartInitialState)
 
 	const setTableData = React.useCallback((chartData) => {
-		const tableData = chartData.data.tabularLabels.map((label, i) => ({
+		const tableData = chartData.data.tabularLabels.map((label: string, i: number) => ({
 			label,
 			value: chartData.data.labelsY[i],
 		}))
 
 		dispatch({ type: SET_TABLE_DATA, payload: tableData })
-	}, [dispatch])
+	}, [])
 
 	const sortTableData = React.useCallback(({ key, orderBy }) => {
 		dispatch({ type: UPDATE_SORT, payload: { key, orderBy } })
-	}, [dispatch])
+	}, [])
 
 	return {
 		state,
