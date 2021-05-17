923c0316355f4b1d97b4e8a3cbe6c12f7aad06ad
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-05-17 10:53:58 +0300

    [RT-38] add: oc-app-short-info to /molecules

---
+++ src/ui/market/molecules/oc-app-short-info/oc-app-short-info.tsx
@@ -0,0 +1,80 @@
+//commit 06a61466bd7e0307fd39a60ef51e8a8ef6b3eee8 Author: Pavel Date: 14.04.21, 14:34
+import * as React from 'react';
+import { sanitizeUrl } from '@braintree/sanitize-url';
+
+import { Rating } from '../../atoms';
+import { parsePrice } from './utils';
+import { OcAppShortInfoProps } from './types';
+import { stripHtmlTags, isStorybook } from '../../../../lib';
+import './styles.scss';
+
+
+const defaultAppIcon = isStorybook() ? './img/standard-app-icon.svg' : '../../../../assets/img/standard-app-icon.svg';
+
+export const OcAppShortInfo: React.FC<OcAppShortInfoProps> = (props) => {
+	const {
+		app,
+		clickByApp,
+		dropdown,
+	} = props;
+
+	const {
+		icon,
+		name,
+		model,
+		rating,
+		reviewCount,
+		summary,
+		description,
+	} = app;
+
+	const onNameClick = () => {
+		clickByApp(app);
+	};
+
+	const appDescription = stripHtmlTags(summary ? summary : description);
+
+	const appIcon = React.useMemo(() => {
+		const url = sanitizeUrl(icon);
+
+		return url !== 'about:blank' ? url : defaultAppIcon;
+	}, [icon]);
+
+	return (
+		<div className="card-wrapper">
+			<div className="info-card">
+				<div className="info-card__logo">
+					<img src={appIcon} alt={name} className="info-card__logo-image" />
+				</div>
+				<div className="info-card__content">
+					<div className="info-card__content-wrapper">
+						{/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex,jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions */}
+						<p className="info-card__content-name" /*ellipsis*/ tabIndex={0} onClick={onNameClick}>{name}</p>
+						<div className="info-card__dropdown info-card__dropdown_mobile">
+							{dropdown}
+						</div>
+					</div>
+					<div className="info-card__content-data">
+						<span className="info-card__content-price">{parsePrice(model[0])}</span>
+						<Rating
+							type="single-star"
+							rating={Number(rating) * 0.01}
+							reviewCount={reviewCount}
+							className="info-card__content-rating"
+						/>
+					</div>
+					<p
+						className="info-card__content-summary"
+						title={appDescription}
+						// ellipsis
+					>
+						{appDescription}
+					</p>
+				</div>
+			</div>
+			<div className="info-card__dropdown info-card__dropdown_desktop">
+				{dropdown}
+			</div>
+		</div>
+	)
+}
---
+++ src/ui/market/molecules/oc-app-short-info/utils.ts
@@ -0,0 +1,24 @@
+import { AppModel } from './types';
+
+
+const isoCurrencyCodes = new Map([
+	[ 'USD', '$' ],
+	[ 'EUR', '€' ],
+	[ 'CNY', '¥' ],
+	[ 'GBP', '£' ],
+]);
+
+export const parsePrice = ({ type, currency, price, billingPeriod }: AppModel): string => {
+	if (type === 'free') {
+		return 'Free';
+	}
+
+	const parsedCurrency = currency ? isoCurrencyCodes.has(currency) ? isoCurrencyCodes.get(currency) : '$' : '';
+	let parsedPrice = `${parsedCurrency}${price / 100}`;
+
+	if (billingPeriod) {
+		parsedPrice = parsedPrice.concat(`/${billingPeriod.substring(0, 2)}`);
+	}
+
+	return parsedPrice;
+};
