536ce9508f47c9a64196ca6cd2b61373846fb5f7
serhdemidov <serhdemidov@softkit.company>
2021-05-24 12:33:45 +0300

    Fixed cursor input bug

--- src/ui/common/molecules/oc-datetime-picker/icon-input.tsx
+++ src/ui/common/molecules/oc-datetime-picker/icon-input.tsx
@@ -9,7 +9,7 @@ export const InputWithIcon = (
     React.SVGAttributes<SVGElement>,
 ) => (
   <div className="inputWithIcon">
-    <input {...props} className="date-input" contentEditable={false} />
+    <input {...props} className="date-input" contentEditable={false} onChange={undefined} />
     <CalendarIcon className="calendarIcon" onClick={props.disabled ? undefined : props.onClick} />
   </div>
 );
