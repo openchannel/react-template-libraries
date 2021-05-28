7ee247e51d85b53f9282408294adcca0a84f247f
serhdemidov <serhdemidov@softkit.company>
2021-05-28 13:53:25 +0300

    update: test

--- test/common/molecules/oc-video-url/oc-video-url.test.tsx
+++ test/common/molecules/oc-video-url/oc-video-url.test.tsx
@@ -1,52 +1,34 @@
 import * as React from 'react';
-import enzyme, { shallow, ShallowWrapper } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { shallow, mount, ShallowWrapper } from 'enzyme';
 
-import {
-	OcVideoUrlComponent,
-	VideoUrlProps,
-} from '../../../../src/ui/common/molecules/oc-video-url';
+import { OcVideoUrlComponent } from '../../../../src/ui/common/molecules/oc-video-url';
 
-enzyme.configure({ adapter: new Adapter() });
-
-const defaultProps: VideoUrlProps = {
-	placeholder: 'Enter your video url here',
-	videoUrl: 'https://www.youtube.com/watch?v=DGQwd1_dpuc',
-	withoutPreview: false,
-	onChange: (e) => e.target.value,
-};
-
-const setUp = (props: VideoUrlProps) => shallow(<OcVideoUrlComponent {...props} />);
+const component: ShallowWrapper = shallow(
+	<OcVideoUrlComponent
+		placeholder="Enter your video url here"
+		videoUrl="https://www.youtube.com/watch?v=DGQwd1_dpuc"
+		withoutPreview={false}
+		disabled={false}
+		onChange={(e) => e.target.value}
+	/>,
+);
 
 describe('Video Url component', () => {
-	let component: ShallowWrapper;
-
-	beforeEach(() => {
-		component = setUp(defaultProps);
-	});
-
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
 
 	it('should contain placeholder', () => {
-		expect(component.prop('placeholder')).toEqual('Enter your video url here');
-	});
-
-	it('should be disabled', async () => {
-		component.setProps({ disabled: true });
-
-		expect(component.prop('disabled')).toBeTruthy();
-	});
-
-	it('should click', async () => {
-		const onButtonClickMock = jest.fn();
-
-		component.setProps({ onClick: onButtonClickMock });
-		component.simulate('click');
-
-		expect(onButtonClickMock).toHaveBeenCalledTimes(1);
+		const wrapper = mount(
+			<OcVideoUrlComponent
+				placeholder="Enter your video url here"
+				videoUrl="https://www.youtube.com/watch?v=DGQwd1_dpuc"
+				withoutPreview={false}
+				disabled={false}
+				onChange={(e) => e.target.value}
+			/>,
+		);
+		expect(wrapper.prop('placeholder')).toEqual('Enter your video url here');
+		wrapper.unmount();
 	});
 });
-
-//write test to the end
