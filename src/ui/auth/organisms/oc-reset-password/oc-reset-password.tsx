5fcda708490ababe1b48c1db2f43e6c632ab4a93
serhdemidov <serhdemidov@softkit.company>
2021-06-02 15:39:10 +0300

    update: story, test, component

---
+++ src/ui/auth/organisms/oc-reset-password/oc-reset-password.tsx
@@ -0,0 +1,79 @@
+// commit 8042d87b78e1a50f433116e39cb0a61c0a457b9f Author: Julia Date: 26.02.21, 19:40
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+import {
+	OcButtonComponent,
+	OcError,
+	OcLabelComponent,
+	OcPasswordComponent,
+} from '../../../../ui/common';
+
+import { OcResetPasswordProps } from './types';
+
+import './styles.scss';
+
+export const OcResetPasswordComponent: React.FC<OcResetPasswordProps> = (props) => {
+	const {
+		companyLogoUrl,
+		loginUrl,
+		signupUrl,
+		process,
+		inputProps,
+		inputError,
+		value,
+		onChange,
+	} = props;
+
+	return (
+		<div className="reset-password login-card login-card_borders">
+			{/*<form noValidate>*/}
+			<div className="reset-password__card-body">
+				<div className="reset-password__logo">
+					<img src={companyLogoUrl} className="company-logo" alt="logo" />
+				</div>
+				<div className="reset-password__header">
+					<h4 className="reset-password__header-heading">New Password</h4>
+					<OcLabelComponent>Create new password</OcLabelComponent>
+				</div>
+				<div className="reset-password__password-group">
+					<span className="reset-password__password-group-label">
+						<OcLabelComponent htmlFor={inputProps?.id}>New Password</OcLabelComponent>
+					</span>
+					<OcPasswordComponent
+						disabled={false}
+						placeholder="New Password"
+						value={value}
+						onChange={onChange}
+						required
+						{...inputProps}
+					/>
+					{inputError && <OcError message={String(inputError)} />}
+				</div>
+				<OcButtonComponent
+					htmlType="submit"
+					type="primary"
+					customClass="reset-password__button"
+					process={process}
+				>
+					Confirm
+				</OcButtonComponent>
+				<div className="reset-password__log-in">
+					<OcLabelComponent>Already have an account?</OcLabelComponent>
+					<Link to={loginUrl} className="reset-password__link">
+						{' '}
+						Log In
+					</Link>
+				</div>
+				<div className="reset-password__sign-up">
+					<OcLabelComponent>Don't have an account yet?</OcLabelComponent>
+					<Link to={signupUrl} className="reset-password__link">
+						{' '}
+						Sign Up
+					</Link>
+				</div>
+			</div>
+			{/*</form>*/}
+		</div>
+	);
+};
---
+++ stories/auth/organisms/oc-reset-password.stories.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Story, Meta } from '@storybook/react';
+
+import { errorMessages } from '../../../src/lib/validation';
+import { OcResetPasswordComponent, OcResetPasswordProps } from '../../../src/ui/auth/index';
+
+export default {
+	title: 'Reset Password [BEM]',
+	component: OcResetPasswordComponent,
+} as Meta;
+
+const DefaultComponent: Story<OcResetPasswordProps> = (args) => (
+	<BrowserRouter>
+		<OcResetPasswordComponent {...args} />
+	</BrowserRouter>
+);
+
+export const Default = DefaultComponent.bind({});
+Default.args = {
+	companyLogoUrl: './img/logo-company.png',
+	resendActivationUrl: '/',
+	signupUrl: '/',
+	inputProps: {
+		id: 'input',
+	},
+};
+
+const FilledComponent: Story<OcResetPasswordProps> = (args) => {
+	const [value, setValue] = React.useState('testpassword');
+	const [blurred, setBlurred] = React.useState(false);
+
+	return (
+		<BrowserRouter>
+			<OcResetPasswordComponent
+				{...args}
+				inputProps={{
+					id: 'input',
+					value,
+					onChange: (e) => setValue(e.target.value),
+					onBlur: () => setBlurred(true),
+				}}
+				inputError={blurred && !value && errorMessages.required()}
+			/>
+		</BrowserRouter>
+	);
+};
+
+export const Filled = FilledComponent.bind({});
+Filled.args = {
+	companyLogoUrl: './img/logo-company.png',
+	loginUrl: '/',
+	signupUrl: '/',
+};
