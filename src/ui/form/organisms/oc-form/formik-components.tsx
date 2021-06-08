8f28e512a863f92630ef04137dadcef380541b5b
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-06-08 09:25:51 +0300

    [RT-47] upd: add actions to main form. create wrapped components for formik

---
+++ src/ui/form/organisms/oc-form/formik-components.tsx
@@ -0,0 +1,157 @@
+import * as React from 'react';
+import { useFormikContext } from 'formik';
+import { OcDatetimePicker } from '../../../common';
+import { OcVideoUrlComponent } from '../../../common';
+import { OcCheckboxComponent } from '../../../common';
+import { OcFileUpload } from '../../../common';
+
+import { OcSelect } from '../../../common';
+import { OcRichTextEditorComponent } from '../../../common';
+
+import { OcTooltipLabel } from '../../atoms';
+import { OcColorComponent, OcError } from '../../../common';
+import { FIELD_TYPE } from '../../lib';
+import { OcMultiSelectList } from '../../molecules/oc-multi-select-list';
+import { OcTags } from '../../molecules/oc-tags';
+
+export const FormGroup = (props: any) => {
+	const {
+		children,
+		error,
+		label,
+		labelFor,
+		description,
+		required,
+	} = props;
+
+	return (
+		<>
+			{label && (
+				<div className="form__field-label">
+					<OcTooltipLabel
+						htmlFor={labelFor}
+						required={required}
+						description={description}
+					>
+						{label}
+					</OcTooltipLabel>
+				</div>
+			)}
+			<div className="form__field-input">
+				{children}
+			</div>
+			{error && <OcError message={error} />}
+		</>
+	);
+};
+
+export const FormGroupWrapper = (props: any) => {
+	const formik = useFormikContext();
+	const { error, touched } = formik.getFieldMeta(props.name);
+
+	return (
+		<div className="form__field">
+			<FormGroup {...props} error={touched && !!error && error} />
+		</div>
+	);
+};
+
+export const FormikFieldWrapper = ({ field, form, ...props }) => {
+	const onChange = React.useCallback((value) => {
+		form.setFieldValue(field.name, value);
+	}, [form.setFieldValue]);
+
+	return (
+		<OcColorComponent colorValue={field.value} onValueChange={onChange} />
+	)
+}
+
+export const FormikRichTextWrapper = ({ field, form, ...props }) => {
+	const onChange = React.useCallback((value) => {
+		form.setFieldValue(field.name, value);
+	}, [form.setFieldValue]);
+
+	return (
+		<OcRichTextEditorComponent
+			onChange={onChange}
+			value={field.value}
+			placeholderText={props.placeholder}
+		/>
+	);
+};
+
+export const FormikOcSelectWrapper = ({ field, form, ...props }) => {
+	const onChange = React.useCallback((value) => {
+		form.setFieldValue(field.name, value);
+	}, [form.setFieldValue]);
+
+	return (
+		<OcSelect
+			selectValArr={props.options}
+			value={field.value}
+			onSelectionChange={onChange}
+			placeholder={props.placeholder}
+		/>
+	);
+};
+
+export const FormikOcTagsWrapper = ({ field, form, ...props }) => {
+	const onChange = React.useCallback((value) => {
+		form.setFieldValue(field.name, value);
+	}, [form.setFieldValue]);
+
+	return (
+		<OcTags
+			availableTags={props.options}
+			value={field.value}
+			onChange={onChange}
+			placeholder={props.placeholder}
+			tagsType={props.tagsType}
+		/>
+	);
+};
+
+export const FormikOcVideoUrlWrapper = ({ field, form, ...props }) => {
+	const onChange = React.useCallback((value) => {
+		form.setFieldValue(field.name, value);
+	}, [form.setFieldValue]);
+
+	return (
+		<OcVideoUrlComponent
+			value={field.value}
+			onChange={onChange}
+			placeholder={props.placeholder}
+		/>
+	);
+};
+
+export const FormikOcDatetimePickerWrapper = ({ field, form, ...props }) => {
+	const onChange = React.useCallback((value) => {
+		form.setFieldValue(field.name, value);
+	}, [form.setFieldValue]);
+
+	return (
+		<OcDatetimePicker
+			type={props.type}
+			value={field.value}
+			onChange={onChange}
+			disabled={props.disabled}
+			{...(props.type === FIELD_TYPE.DATE ? { settings: 'dd/MM/yyyy' } : {})}
+		/>
+	);
+};
+
+export const FormikOcMultiSelectListWrapper = ({ field, form, ...props }) => {
+	const onChange = React.useCallback((value) => {
+		form.setFieldValue(field.name, value);
+	}, [form.setFieldValue]);
+
+	return (
+		<OcMultiSelectList
+			label={props.label}
+			availableItemsList={props.options}
+			value={field.value}
+			onChange={onChange}
+		/>
+	);
+};
--- src/ui/form/organisms/oc-form/oc-form.tsx
+++ src/ui/form/organisms/oc-form/oc-form.tsx
@@ -6,10 +6,25 @@ import {
 	Form as FormikForm,
 	Field,
 } from 'formik';
+import _ from 'lodash';
+import { OcPasswordComponent } from '../../../common';
+import { OcVideoUrlComponent } from '../../../common';
+import { OcCheckboxComponent } from '../../../common';
+import { OcNumberComponent } from '../../../common';
+import { OcButtonComponent } from '../../../common';
 
 import { OcTooltipLabel } from '../../atoms';
 import { FIELD_TYPE } from '../../lib';
 import { OcDynamicFieldArray } from '../oc-dynamic-field-array';
+import { OcInputComponent } from '../../../common';
+import { FormikOcMultiSelectListWrapper } from './formik-components';
+import { FormikOcDatetimePickerWrapper } from './formik-components';
+import { FormikOcVideoUrlWrapper } from './formik-components';
+import { FormikOcTagsWrapper } from './formik-components';
+import { FormikFieldWrapper } from './formik-components';
+import { FormikOcSelectWrapper } from './formik-components';
+import { FormikRichTextWrapper } from './formik-components';
+import { FormGroupWrapper } from './formik-components';
 import { OcColorComponent, OcError, OcInputComponent } from '../../../common';
 
 import { AppFormModel } from './types';
@@ -58,6 +73,63 @@ const formJsonData: AppFormModel = {
 					label: 'field1',
 					placeholder: 'write some text',
 					type: 'text',
+				},
+				{
+					attributes: {
+						required: true,
+					},
+					category: 'CUSTOM',
+					defaultValue: null,
+					description: null,
+					id: 'test-color-component',
+					isOpen: false,
+					isValid: true,
+					deleteable: false,
+					label: 'Test Color Component',
+					placeholder: 'Choose your color',
+					type: 'color',
+				},
+				{
+					id: 'role',
+					label: 'role',
+					description: '',
+					defaultValue: 'user',
+					type: 'dropdownList',
+					required: null,
+					attributes: { required: true },
+					options: ['admin', 'user', 'test'],
+					subFieldDefinitions: null,
+				},
+				{
+					attributes: {
+						required: true,
+						maxCount: 3,
+						minCount: 2,
+					},
+					options: ['One', 'Two', 'Three', 'Five'],
+					category: 'CUSTOM',
+					defaultValue: [],
+					description: null,
+					id: 'multi-select-test',
+					isOpen: false,
+					isValid: true,
+					deleteable: false,
+					label: 'Multi Select test',
+					placeholder: 'select some',
+					type: 'multiselectList',
+				},
+				{
+					attributes: {
+						max: 25,
+						min: 5,
+						required: null,
+					},
+					defaultValue: null,
+					description: '',
+					id: 'test-number',
+					label: 'Test number',
+					placeholder: null,
+					type: 'number',
 				},
 				{
 					id: 'long-text-example',
@@ -154,58 +226,6 @@ const formJsonData: AppFormModel = {
 	],
 };
 
-const FormikFieldWrapper = ({ field, form, ...props }) => {
-	const onChange = React.useCallback((value) => {
-		form.setFieldValue(field.name, value);
-	}, [form.setFieldValue]);
-
-	return (
-		<OcColorComponent colorValue={field.value} onValueChange={onChange} />
-	)
-}
-
-const FormGroup = (props: any) => {
-	const {
-		children,
-		error,
-		label,
-		labelFor,
-		description,
-		required,
-	} = props;
-
-	return (
-		<>
-			{label && (
-				<div className="form__field-label">
-					<OcTooltipLabel
-						htmlFor={labelFor}
-						required={required}
-						description={description}
-					>
-						{label}
-					</OcTooltipLabel>
-				</div>
-			)}
-			<div className="form__field-input">
-				{children}
-			</div>
-			{error && <OcError message={error} />}
-		</>
-	);
-}
-
-const FormGroupWrapper = (props: any) => {
-	const formik = useFormikContext();
-	const { error, touched } = formik.getFieldMeta(props.name);
-
-	return (
-		<div className="form__field">
-			<FormGroup {...props} error={touched && !!error && error} />
-		</div>
-	)
-}
-
 export const RecursiveContainer: any = ({ fields }) => {
 	const builder = (element, index) => {
 		const {
@@ -215,7 +235,7 @@ export const RecursiveContainer: any = ({ fields }) => {
 			type,
 			name,
 			attributes,
-			// options,
+			options,
 			// subFieldDefinitions,
 			// fields,
 			placeholder,
@@ -223,6 +243,22 @@ export const RecursiveContainer: any = ({ fields }) => {
 		} = element;
 
 		switch (type) {
+			case FIELD_TYPE.RICH_TEXT:
+				return (
+					<FormGroupWrapper
+						name={name}
+						label={label}
+						labelFor={id}
+						description={description}
+						required={attributes.required}
+					>
+						<Field
+							name={name}
+							component={FormikRichTextWrapper}
+							placeholder={placeholder}
+						/>
+					</FormGroupWrapper>
+				);
 			case FIELD_TYPE.TEXT:
 				return (
 					<FormGroupWrapper
@@ -242,10 +278,99 @@ export const RecursiveContainer: any = ({ fields }) => {
 						/>
 					</FormGroupWrapper>
 				);
-			case 'number':
+			case FIELD_TYPE.DROPDOWN_LIST:
 				return (
-					'number input'
-				)
+					<FormGroupWrapper
+						name={name}
+						label={label}
+						labelFor={id}
+						description={description}
+						required={attributes.required}
+					>
+						<Field
+							name={name}
+							component={FormikOcSelectWrapper}
+							placeholder={placeholder}
+							required={attributes.required}
+							options={options}
+						/>
+					</FormGroupWrapper>
+				);
+			case FIELD_TYPE.NUMBER:
+				return (
+					<FormGroupWrapper
+						name={name}
+						label={label}
+						labelFor={id}
+						description={description}
+						required={attributes.required}
+					>
+						<Field
+							name={name}
+							as={OcNumberComponent}
+							placeholder={placeholder}
+							required={attributes.required}
+							id={id}
+						/>
+					</FormGroupWrapper>
+				);
+			case FIELD_TYPE.CHECKBOX:
+				return (
+					<FormGroupWrapper
+						name={name}
+						label={label}
+						labelFor={id}
+						description={description}
+						required={attributes.required}
+					>
+						<Field
+							name={name}
+							as={OcCheckboxComponent}
+							placeholder={placeholder}
+							required={attributes.required}
+							// labelText={}
+							id={id}
+						/>
+					</FormGroupWrapper>
+				);
+			case FIELD_TYPE.EMAIL_ADDRESS:
+				return (
+					<FormGroupWrapper
+						name={name}
+						label={label}
+						labelFor={id}
+						description={description}
+						required={attributes.required}
+					>
+						<Field
+							name={name}
+							as={OcInputComponent}
+							placeholder={placeholder || 'myemail@example.com'}
+							required={attributes.required}
+							inputType="email"
+							id={id}
+						/>
+					</FormGroupWrapper>
+				);
+			case FIELD_TYPE.WEBSITE_URL:
+				return (
+					<FormGroupWrapper
+						name={name}
+						label={label}
+						labelFor={id}
+						description={description}
+						required={attributes.required}
+					>
+						<Field
+							name={name}
+							as={OcInputComponent}
+							placeholder={placeholder || 'https://my.website.com'}
+							required={attributes.required}
+							inputType="url"
+							id={id}
+						/>
+					</FormGroupWrapper>
+				);
 			case FIELD_TYPE.COLOR:
 				return (
 					<FormGroupWrapper
@@ -263,8 +388,10 @@ export const RecursiveContainer: any = ({ fields }) => {
 							{...attributes}
 						/>
 					</FormGroupWrapper>
-				)
-			case FIELD_TYPE.DYNAMIC_FIELD_ARRAY:
+				);
+			case FIELD_TYPE.TAGS:
+			case FIELD_TYPE.BOOLEAN_TAGS:
+			case FIELD_TYPE.NUMBER_TAGS:
 				return (
 					<FormGroupWrapper
 						name={name}
@@ -273,12 +400,119 @@ export const RecursiveContainer: any = ({ fields }) => {
 						description={description}
 						required={attributes.required}
 					>
-						<OcDynamicFieldArray
-							element={element}
-							showAddButton={fields.length === 0 || fields.length === (index + 1)}
+						<Field
+							id={id}
+							name={name}
+							component={FormikOcTagsWrapper}
+							placeholder={placeholder}
+							options={options}
+							tagsType={
+								type === FIELD_TYPE.BOOLEAN_TAGS
+									? 'boolean'
+									: type === FIELD_TYPE.NUMBER_TAGS
+									? 'number'
+									: 'string'
+							}
 						/>
 					</FormGroupWrapper>
 				);
+			case FIELD_TYPE.VIDEO_URL:
+				return (
+					<FormGroupWrapper
+						name={name}
+						label={label}
+						labelFor={id}
+						description={description}
+						required={attributes.required}
+					>
+						<Field
+							id={id}
+							name={name}
+							component={FormikOcVideoUrlWrapper}
+							placeholder={placeholder}
+						/>
+					</FormGroupWrapper>
+				);
+			case FIELD_TYPE.DATE:
+			case FIELD_TYPE.DATE_TIME:
+				return (
+					<FormGroupWrapper
+						name={name}
+						label={label}
+						labelFor={id}
+						description={description}
+						required={attributes.required}
+					>
+						<Field
+							id={id}
+							name={name}
+							component={FormikOcDatetimePickerWrapper}
+							type={type}
+						/>
+					</FormGroupWrapper>
+				);
+			case FIELD_TYPE.MULTISELECT_LIST:
+				return (
+					<FormGroupWrapper
+						name={name}
+						label={label}
+						labelFor={id}
+						description={description}
+						required={attributes.required}
+					>
+						<Field
+							id={id}
+							name={name}
+							component={FormikOcMultiSelectListWrapper}
+							options={options}
+							label={label}
+						/>
+					</FormGroupWrapper>
+				);
+			case FIELD_TYPE.PASSWORD:
+				return (
+					<FormGroupWrapper
+						name={name}
+						label={label}
+						labelFor={id}
+						description={description}
+						required={attributes.required}
+					>
+						<Field name={name} as={OcPasswordComponent} />
+					</FormGroupWrapper>
+				);
+			case FIELD_TYPE.DYNAMIC_FIELD_ARRAY: {
+				const group = _.groupBy(fields, 'type')[FIELD_TYPE.DYNAMIC_FIELD_ARRAY];
+				const isFirst = group[0].fields.length === 0 || group[0].index === fields[index].index;
+
+				const groupFieldIndex = group.findIndex((g) => g.index === index);
+
+				// render label only for first element of this type
+				if (isFirst) {
+					return (
+						<FormGroupWrapper
+							name={name}
+							label={label}
+							labelFor={id}
+							description={description}
+							required={attributes.required}
+						>
+						<OcDynamicFieldArray
+							element={element}
+							groupFieldIndex={groupFieldIndex}
+							showAddButton={fields.length === 0 || fields.length === (index + 1)}
+						/>
+						</FormGroupWrapper>
+					);
+				}
+				return (
+					<OcDynamicFieldArray
+						element={element}
+						groupFieldIndex={groupFieldIndex}
+						showAddButton={fields.length === 0 || fields.length === (index + 1)}
+					/>
+				);
+			}
 			default:
 				return <div>Unsupported field</div>
 		}
@@ -289,83 +523,6 @@ export const RecursiveContainer: any = ({ fields }) => {
 	);
 }
 
-export const FieldDeterminant: any = (element) => {
-	const {
-		id,
-		label,
-		description,
-		type,
-		name,
-		attributes,
-		// options,
-		// subFieldDefinitions,
-		// fields,
-		placeholder,
-		// category,
-	} = element;
-
-	switch (type) {
-		case FIELD_TYPE.TEXT:
-			return (
-				<FormGroupWrapper
-					name={name}
-					label={label}
-					labelFor={id}
-					description={description}
-					required={attributes.required}
-				>
-					<Field
-						name={name}
-						as={OcInputComponent}
-						placeholder={placeholder}
-						required={attributes.required}
-						id={id}
-						inputType="text"
-					/>
-				</FormGroupWrapper>
-			);
-		case 'number':
-			return (
-				'number input'
-			)
-		case FIELD_TYPE.COLOR:
-			return (
-				<FormGroupWrapper
-					name={name}
-					label={label}
-					labelFor={id}
-					description={description}
-					required={attributes.required}
-				>
-					<Field
-						name={name}
-						component={FormikFieldWrapper}
-						placeholder={placeholder}
-						id={id}
-						{...attributes}
-					/>
-				</FormGroupWrapper>
-			)
-		case FIELD_TYPE.DYNAMIC_FIELD_ARRAY:
-			return (
-				<FormGroupWrapper
-					name={name}
-					label={label}
-					labelFor={id}
-					description={description}
-					required={attributes.required}
-				>
-					<OcDynamicFieldArray
-						element={element}
-						fields={element.fields}
-					/>
-				</FormGroupWrapper>
-			);
-		default:
-			return <div>Unsupported field</div>
-	}
-};
-
 const RecursiveContainerWrapper = () => {
 	const context = useOcFormContext();
 
@@ -374,8 +531,16 @@ const RecursiveContainerWrapper = () => {
 	)
 }
 
+const getOcFormButtonsClass = (buttonPosition: string): string => {
+	switch (buttonPosition) {
+		case 'center': return `form__buttons form__buttons_justify_center`;
+		case 'left': return `form__buttons form__buttons_justify_start`;
+		default: return `form__buttons form__buttons_justify_start form__buttons_direction_row_reverse`;
+	}
+};
+
 export const OcForm: React.FC<any> = (props) => {
-	const { data } = props;
+	const { data, successButtonText = 'Submit', buttonPosition = 'left' } = props;
 
 	const onSubmit = React.useCallback((...args) => {
 		console.log('args', ...args)
@@ -392,13 +557,25 @@ export const OcForm: React.FC<any> = (props) => {
 	});
 
 	// console.log('fields', fields)
-	console.log('formik.values', formik.values)
+	// console.log('formik.values', formik.values)
 
 	return (
 		<FormikContext.Provider value={formik}>
 			<OcFormContextProvider initialValue={{ fields }}>
 				<FormikForm className="form">
 					<RecursiveContainerWrapper />
+					<div className={getOcFormButtonsClass(buttonPosition)}>
+						<div className="form__button">
+							<OcButtonComponent type="primary">
+								{successButtonText}
+							</OcButtonComponent>
+						</div>
+						<div className="form__button">
+							<OcButtonComponent type="secondary">
+								Cancel
+							</OcButtonComponent>
+						</div>
+					</div>
 				</FormikForm>
 			</OcFormContextProvider>
 		</FormikContext.Provider>
