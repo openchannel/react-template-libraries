32c857787b90e6d78b1a5fb8fdbbe06e31e6422f
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-06-08 09:25:10 +0300

    [RT-00] fix: code-style, props, types of reusable components

--- src/ui/common/molecules/oc-video-url/index.tsx
+++ src/ui/common/molecules/oc-video-url/index.tsx
@@ -1,13 +1,9 @@
 //commit 91e34d420898eff90181614319cb86372907fafc Author: Alex Tkachenko Date: 08.10.20, 11:46
 import * as React from 'react';
-import { OcVideoComponent, OcInputComponent, VideoProps, InputProps } from '../../index';
+import { OcVideoComponent, OcInputComponent, InputProps } from '../../index';
 import './style.scss';
 
-export interface VideoUrlProps extends VideoProps, InputProps {
-	/**
-	 * initial input value
-	 */
-	videoUrl: string;
+export interface VideoUrlProps extends InputProps {
 	/**
 	 * value - useState input value
 	 */
@@ -19,15 +15,17 @@ export interface VideoUrlProps extends VideoProps, InputProps {
 	/**
 	 * withoutPreview - boolean value, if you don't want preview video
 	 */
-	withoutPreview: boolean;
+	withoutPreview?: boolean;
 }
+
 export const validateURL = (str: string) => {
 	const pattern = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
 	return pattern.test(str);
 };
 
 export const OcVideoUrlComponent: React.FC<VideoUrlProps> = (props) => {
-	const { disabled, placeholder, customClass, onChange, withoutPreview, value } = props;
+	const { customClass, onChange, withoutPreview = false, value, ...p } = props;
+
 	const handleChange = React.useCallback(
 		(e: any) => {
 			onChange(e.target.value);
@@ -39,11 +37,10 @@ export const OcVideoUrlComponent: React.FC<VideoUrlProps> = (props) => {
 		<>
 			<OcInputComponent
 				inputType="text"
-				disabled={disabled}
-				placeholder={placeholder}
 				value={value}
 				onChange={handleChange}
 				className={`form-control ${customClass}`}
+				{...p}
 			/>
 			{!withoutPreview && validateURL(value) && (
 				<OcVideoComponent customClass="video-url__reference" videoUrl={value} />
