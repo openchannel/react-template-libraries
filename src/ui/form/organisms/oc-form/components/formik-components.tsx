44843d6d50a116701a041571627948a0812213f0
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-06-17 14:23:38 +0300

    [RT-50] upd: add memo utils. wrap formik-components with react.memo and memo utils

--- src/ui/form/organisms/oc-form/components/formik-components.tsx
+++ src/ui/form/organisms/oc-form/components/formik-components.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useFormikContext } from 'formik';
+import { FieldInputProps, useFormikContext } from 'formik';
 import moment from 'moment';
 
 import {
@@ -17,9 +17,10 @@ import {
 import { OcTooltipLabel } from '../../../atoms';
 import { FIELD_TYPE } from '../../../lib';
 import { OcMultiSelectList, OcMultiSelectListProps, OcTags, OcTagsProps } from '../../../molecules';
-import { FCWP, FieldGroupProps } from '../types';
+import type { FCWP, FieldGroupProps } from '../types';
+import { shouldFieldGroupUpdate, shouldFieldUpdate } from '../utils/memo';
 
-export const FieldGroup: React.FC<FieldGroupProps & { error?: string }> = (props) => {
+export const FieldGroup: React.FC<FieldGroupProps & { error?: string }> = React.memo((props) => {
 	const { children, error, label, labelFor, description, required } = props;
 
 	return (
@@ -35,7 +36,7 @@ export const FieldGroup: React.FC<FieldGroupProps & { error?: string }> = (props
 			{error && <OcError message={error} />}
 		</>
 	);
-};
+}, shouldFieldGroupUpdate);
 
 export const FieldGroupWrapper: React.FC<FieldGroupProps> = (props) => {
 	const { name } = props;
@@ -49,27 +50,32 @@ export const FieldGroupWrapper: React.FC<FieldGroupProps> = (props) => {
 	);
 };
 
-export const FormikOcColoWrapper: React.FC<FCWP<ColorProps['colorValue']>> = ({ field, form }) => {
-	const onChange = React.useCallback(
-		(value) => {
-			form.setFieldValue(field.name, value);
-		},
-		[form.setFieldValue],
-	);
+export const FormikOcColoWrapper: React.FC<FCWP<ColorProps['colorValue']>> = React.memo(
+	({ field, form }) => {
+		const onChange = React.useCallback(
+			(value) => {
+				form.setFieldValue(field.name, value);
+			},
+			[form.setFieldValue],
+		);
 
-	return (
-		<OcColorComponent
-			colorValue={field.value || ''}
-			onValueChange={onChange}
-			onBlur={field.onBlur}
-			name={field.name}
-		/>
-	);
-};
+		return (
+			<OcColorComponent
+				colorValue={field.value || ''}
+				onValueChange={onChange}
+				onBlur={field.onBlur}
+				name={field.name}
+			/>
+		);
+	},
+	shouldFieldUpdate(),
+);
 
 export const FormikRichTextWrapper: React.FC<
-	FCWP<string | undefined> & { placeholder: string }
-> = ({ field, form, placeholder }) => {
+	FCWP<string | undefined> & {
+		placeholder: string;
+	}
+> = React.memo(({ field, form, placeholder }) => {
 	const onChange = React.useCallback(
 		(value) => {
 			form.setFieldValue(field.name, value);
@@ -79,40 +85,37 @@ export const FormikRichTextWrapper: React.FC<
 
 	return (
 		<OcRichTextEditorComponent
+			name={field.name}
 			onChange={onChange}
 			value={field.value}
 			placeholderText={placeholder}
+			onBlur={field.onBlur}
 		/>
 	);
-};
+}, shouldFieldUpdate(['placeholder']));
 
 export const FormikOcSelectWrapper: React.FC<
 	FCWP<OcSelectProps['value'] | undefined> & {
 		options: OcSelectProps['selectValArr'];
 		placeholder: OcSelectProps['placeholder'];
 	}
-> = React.memo(
-	({ field, form, options, placeholder }) => {
-		const onChange = React.useCallback(
-			(value) => {
-				form.setFieldValue(field.name, value);
-			},
-			[form.setFieldValue],
-		);
+> = React.memo(({ field, form, options, placeholder }) => {
+	const onChange = React.useCallback(
+		(value) => {
+			form.setFieldValue(field.name, value);
+		},
+		[form.setFieldValue],
+	);
 
-		return (
-			<OcSelect
-				selectValArr={options}
-				value={field.value}
-				onSelectionChange={onChange}
-				placeholder={placeholder}
-			/>
-		);
-	},
-	(prevProps, nextProps) => {
-		return prevProps.field.value === nextProps.field.value;
-	},
-);
+	return (
+		<OcSelect
+			selectValArr={options}
+			value={field.value}
+			onSelectionChange={onChange}
+			placeholder={placeholder}
+		/>
+	);
+}, shouldFieldUpdate(['options', 'placeholder']));
 
 export const FormikOcTagsWrapper: React.FC<
 	FCWP<OcTagsProps['value']> & {
@@ -120,7 +123,7 @@ export const FormikOcTagsWrapper: React.FC<
 		placeholder: OcTagsProps['placeholder'];
 		tagsType: OcTagsProps['tagsType'];
 	}
-> = ({ field, form, options, placeholder, tagsType }) => {
+> = React.memo(({ field, form, options, placeholder, tagsType }) => {
 	const onChange = React.useCallback(
 		(value) => {
 			form.setFieldValue(field.name, value);
@@ -130,20 +133,22 @@ export const FormikOcTagsWrapper: React.FC<
 
 	return (
 		<OcTags
+			name={field.name}
 			availableTags={options}
 			value={field.value}
 			onChange={onChange}
 			placeholder={placeholder}
 			tagsType={tagsType}
+			onBlur={field.onBlur}
 		/>
 	);
-};
+}, shouldFieldUpdate(['options', 'placeholder', 'tagsType']));
 
 export const FormikOcVideoUrlWrapper: React.FC<
 	FCWP<VideoUrlProps['value']> & {
 		placeholder: VideoUrlProps['placeholder'];
 	}
-> = ({ field, form, placeholder }) => {
+> = React.memo(({ field, form, placeholder }) => {
 	const onChange = React.useCallback(
 		(value) => {
 			form.setFieldValue(field.name, value);
@@ -152,9 +157,15 @@ export const FormikOcVideoUrlWrapper: React.FC<
 	);
 
 	return (
-		<OcVideoUrlComponent value={field.value || ''} onChange={onChange} placeholder={placeholder} />
+		<OcVideoUrlComponent
+			name={field.name}
+			value={field.value || ''}
+			onChange={onChange}
+			onBlur={field.onBlur}
+			placeholder={placeholder}
+		/>
 	);
-};
+}, shouldFieldUpdate(['placeholder']));
 
 export const FormikOcDatetimePickerWrapper: React.FC<
 	FCWP<DatepickerProps['value']> & {
@@ -165,8 +176,6 @@ export const FormikOcDatetimePickerWrapper: React.FC<
 	({ field, form, type, disabled }) => {
 		const onChange = React.useCallback(
 			(value) => {
-				console.log('value', value);
-				console.log('field.name', field.name);
 				form.setFieldValue(field.name, value);
 			},
 			[form.setFieldValue],
@@ -182,9 +191,17 @@ export const FormikOcDatetimePickerWrapper: React.FC<
 			/>
 		);
 	},
-	(prevProps, nextProps) => {
-		return moment(prevProps.field.value).isSame(nextProps.field.value);
-	},
+	shouldFieldUpdate(
+		['type'],
+		(
+			prevProps: Readonly<
+				React.PropsWithChildren<{ field: FieldInputProps<string | moment.Moment> }>
+			>,
+			nextProps: Readonly<
+				React.PropsWithChildren<{ field: FieldInputProps<string | moment.Moment> }>
+			>,
+		) => moment(prevProps.field.value).isSame(nextProps.field.value),
+	),
 );
 
 export const FormikOcMultiSelectListWrapper: React.FC<
@@ -192,7 +209,7 @@ export const FormikOcMultiSelectListWrapper: React.FC<
 		label: OcMultiSelectListProps['label'];
 		options: OcMultiSelectListProps['availableItemsList'];
 	}
-> = ({ field, form, label, options }) => {
+> = React.memo(({ field, form, label, options }) => {
 	const onChange = React.useCallback(
 		(value) => {
 			form.setFieldValue(field.name, value);
@@ -202,10 +219,12 @@ export const FormikOcMultiSelectListWrapper: React.FC<
 
 	return (
 		<OcMultiSelectList
+			name={field.name}
 			label={label}
 			availableItemsList={options}
 			value={field.value}
 			onChange={onChange}
+			onBlur={field.onBlur}
 		/>
 	);
-};
+}, shouldFieldUpdate(['options']));
