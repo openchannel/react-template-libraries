80acf31d221286663f9a3b9514a377981ad0947e
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-06-30 15:40:33 +0300

    [RT-00] upd: update types. add ability to pass a custom listItem component to oc-dropdown.

--- src/ui/common/molecules/oc-dropdown/oc-dropdown-button.tsx
+++ src/ui/common/molecules/oc-dropdown/oc-dropdown-button.tsx
@@ -1,12 +1,9 @@
 import * as React from 'react';
 
-import { ToggleIndicatorProps } from './common/toggle-indicator';
-import { BaseDropdown, BaseDropdownProps } from './base-dropdown';
+import { BaseDropdown } from './base-dropdown';
+import { OcDropdownButtonProps } from './types';
 
 import './oc-dropdown-button.scss';
-
-export type OcDropdownButtonProps = Omit<BaseDropdownProps, 'variant' | 'title'> &
-	ToggleIndicatorProps;
 
 export const OcDropdownButton: React.FC<OcDropdownButtonProps> = (props) => (
 	<BaseDropdown {...props} variant="block" />
