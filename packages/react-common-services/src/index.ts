acb695854ab01f31af39b17d50855a3aa9b43b06
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-07-13 17:50:29 +0300

    [RT-88] add: init services package

---
+++ packages/react-common-services/src/index.ts
@@ -0,0 +1,73 @@
+/*
+ * Public API Surface of angular-common-services
+ */
+// export * from './oc-ng-common-service.module';
+// export * from './service/http-request-services';
+
+/**
+ * Api models
+ */
+export * from './model/api/account-role.model';
+export * from './model/api/app-data.model';
+export * from './model/api/app-form.model';
+export * from './model/api/app-type.model';
+export * from './model/api/change-password.model';
+export * from './model/api/developer.model';
+export * from './model/api/developer-account.model';
+export * from './model/api/file-details.model';
+export * from './model/api/http-params-encoder.model';
+export * from './model/api/invite-user.model';
+export * from './model/api/login.model';
+export * from './model/api/market.model';
+export * from './model/api/ownership.model';
+export * from './model/api/page.model';
+export * from './model/api/properties.model';
+export * from './model/api/request.model';
+export * from './model/api/review.model';
+export * from './model/api/type.model';
+export * from './model/api/user.model';
+export * from './model/api/user-activation.model';
+export * from './model/api/user-login.model';
+export * from './model/api/user-registration.model';
+export * from './model/api/user-type.model';
+
+/**
+ * Component models
+ */
+export * from './model/components/frontend.model';
+
+/**
+ * Services
+ */
+export * from './lib/service/app-form.service';
+export * from './lib/service/app-type.service';
+export * from './lib/service/apps.service';
+export * from './lib/service/apps-version.service';
+export * from './lib/service/auth-holder.service';
+export * from './lib/service/authentication.service';
+export * from './lib/service/chart-service';
+export * from './lib/service/developer.service';
+export * from './lib/service/developer-account.service';
+export * from './lib/service/developer-account-types.service';
+export * from './lib/service/developer-role.service';
+export * from './lib/service/developer-type.service';
+export * from './lib/service/file-upload-download.service';
+export * from './lib/service/frontend.service';
+export * from './lib/service/invite-user.service';
+export * from './lib/service/market.service';
+export * from './lib/service/native-login.service';
+export * from './lib/service/ownership.service';
+export * from './lib/service/properties.service';
+export * from './lib/service/request.service';
+export * from './lib/service/reviews.service';
+export * from './lib/service/site-config.service';
+export * from './lib/service/statistic.service';
+export * from './lib/service/title.service';
+export * from './lib/service/user-account.service';
+export * from './lib/service/user-account-types.service';
+export * from './lib/service/user-account-types.service';
+export * from './lib/service/user-role.service';
+export * from './lib/service/users.service';
+
+/* Utils */
+export * from './lib/util/type-mapper.util';
---
+++ packages/react-common-services/src/service/http-request-services.ts
@@ -0,0 +1,140 @@
+// import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+// import { Inject, Injectable } from '@angular/core';
+// import { Observable, throwError } from 'rxjs';
+// import { catchError, mergeMap } from 'rxjs/operators';
+
+import { AuthenticationService } from './authentication.service';
+
+/**
+ * Description: Service for setting up site config.<br>
+ * @param {{ headers: HttpHeaders; withCredentials: boolean;}} options (default: `{headers: { 'Content-Type': 'application/json' }, withCredentials: true}`) - default Http options
+ * Methods:
+ * get
+ * post
+ * put
+ * patch
+ * delete
+ */
+export class HttpRequestService {
+	// constructor(private http: HttpClient, @Inject('environment') private environment) {}
+	constructor() {}
+
+	options = {
+		headers: new Headers({ 'Content-Type': 'application/json' }),
+		withCredentials: true,
+	};
+
+	/**
+	 *
+	 * Description: GET request with additional http options
+	 *
+	 * @param {string} url - (required) URL string
+	 * @param {any} options - (optional) any Http options
+	 * @returns {Observable<any>} `Observable<any>`
+	 *
+	 * * ### Example:
+	 *
+	 * get('/users', {observe: 'body', reportProgress: true})
+	 */
+	get(url: string, options?: any): Observable<any> {
+		return this.http.get(this.environment.apiUrl + url, this.mergeHttpOptions(options));
+	}
+
+	/**
+	 *
+	 * Description: POST request with provided body and additional http options
+	 *
+	 * @param {string} url - (required) URL string
+	 * @param {any} body - (required) Http body
+	 * @param {any} options - (optional) any Http options
+	 * @returns {Observable<any>} `Observable<any>`
+	 *
+	 * * ### Example:
+	 *
+	 * post('/users', {name:'User', password: 'password'}, {observe: 'body', reportProgress: true})
+	 */
+	post(url: string, body, options?: any): Observable<any> {
+		return this.reInitCSRF(
+			this.http.post<any>(this.environment.apiUrl + url, body, this.mergeHttpOptions(options)),
+		);
+	}
+
+	/**
+	 *
+	 * Description: PUT request with provided body and additional http options
+	 *
+	 * @param {string} url - (required) URL string
+	 * @param {any} body - (required) Http body
+	 * @param {any} options - (optional) any Http options
+	 * @returns {Observable<any>} `Observable<any>`
+	 *
+	 * * ### Example:
+	 *
+	 * put('/users/sd7yf0987sdhf970', {name:'User', password: 'password'}, {observe: 'body', reportProgress: true})
+	 */
+	put(url: string, body, options?: any): Observable<any> {
+		return this.reInitCSRF(
+			this.http.put(this.environment.apiUrl + url, body, this.mergeHttpOptions(options)),
+		);
+	}
+
+	/**
+	 *
+	 * Description: PATCH request with provided body and additional http options
+	 *
+	 * @param {string} url - (required) URL string
+	 * @param {any} body - (required) Http body
+	 * @param {any} options - (optional) any Http options
+	 * @returns {Observable<any>} `Observable<any>`
+	 *
+	 * * ### Example:
+	 *
+	 * patch('/users/sd7yf0987sdhf970', {name:'User', password: 'password'}, {observe: 'body', reportProgress: true})
+	 */
+	patch(url: string, body, options?: any): Observable<any> {
+		return this.reInitCSRF(
+			this.http.patch(this.environment.apiUrl + url, body, this.mergeHttpOptions(options)),
+		);
+	}
+
+	/**
+	 *
+	 * Description: DELETE request with additional http options
+	 *
+	 * @param {string} url - (required) URL string
+	 * @param {any} options - (optional) any Http options
+	 * @returns {Observable<any>} `Observable<any>`
+	 *
+	 * * ### Example:
+	 *
+	 * delete('/users/sd7yf0987sdhf970', {reportProgress: true})
+	 */
+	delete(url: string, options?: any): Observable<any> {
+		return this.reInitCSRF(
+			this.http.delete(this.environment.apiUrl + url, this.mergeHttpOptions(options)),
+		);
+	}
+
+	private reInitCSRF<T>(request: Observable<T>): Observable<T> {
+		return request.pipe(
+			catchError((error: HttpErrorResponse) => {
+				if (error?.status === 403 && error?.error?.toLowerCase()?.includes('csrf')) {
+					return this.initCSRF().pipe(mergeMap((csrf) => request));
+				} else {
+					return throwError(error);
+				}
+			}),
+		);
+	}
+
+	private initCSRF(): Observable<any> {
+		return this.http.get(
+			this.environment.apiUrl + AuthenticationService.INIT_CSRF_URL,
+			this.options,
+		);
+	}
+
+	private mergeHttpOptions(newOptions: any): any {
+		return newOptions ? { ...this.options, ...newOptions } : this.options;
+	}
+}
