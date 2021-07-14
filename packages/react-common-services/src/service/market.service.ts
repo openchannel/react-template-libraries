51d9b6b42832950d260403ff7d34204a9edd9a56
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-07-14 20:53:41 +0300

    [RT-85] add: config, frotnend, market, native-login services

---
+++ packages/react-common-services/src/service/market.service.ts
@@ -0,0 +1,21 @@
+import { api } from '../lib/api';
+import { MarketModel } from '../model/api/market.model';
+
+const MARKET_URL = 'v2/markets';
+
+/**
+ * Description: API service for getting Market Data.<br>
+ * Endpoints:<br>
+ * GET 'v2/markets/this'<br>
+ */
+export const market = {
+	/**
+	 * Description: Get Current Market Data
+	 * @returns {Promise<MarketModel>} `Promise<MarketModel>`
+	 * * ### Example
+	 * getCurrentMarket()
+	 */
+	getCurrentMarket: (): Promise<MarketModel> => {
+		return api.get(`${MARKET_URL}/this`);
+	},
+}
