67f60a048b76029e11086c8d71385d70facae593
serhdemidov <serhdemidov@softkit.company>
2021-07-16 19:34:30 +0300

    interim state of component

--- src/ui/common/atoms/oc-file-upload/index.tsx
+++ src/ui/common/atoms/oc-file-upload/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import Dropzone, { IDropzoneProps } from 'react-dropzone-uploader';
+import Dropzone, { IDropzoneProps /* getFilesFromEvent */ } from 'react-dropzone-uploader';
 
+import { useModalState } from '../../../../lib/hooks';
 import { InputContent } from './input-content';
 import { Layout } from './layout';
 import { PreviewContent } from './preview-content';
@@ -15,30 +16,58 @@ export const OcFileUpload: React.FC<OcFileUploadProps> = () => {
 	// acceptType,
 	// isMultiFile = false,
 	// } = props;
-
 	// const isFileTypeImage = fileType === 'singleImage' || fileType === 'multiImage';
 	// const accept = acceptType || (isFileTypeImage ? 'image/*' : '*/*');
 
-	const handleSubmit: IDropzoneProps['onSubmit'] = (files, allFiles) => {
+	const { isOpened, closeModal, openModal } = useModalState();
+	const [cropper, setCropper] = React.useState<any>();
+	const [cropData, setCropData] = React.useState();
+
+	const getCropData = () => {
+		if (typeof cropper !== 'undefined') {
+			setCropData(cropper.getCroppedCanvas().toDataURL());
+			console.log(cropper);
+		}
+	};
+
+	const handleFileSubmit: IDropzoneProps['onSubmit'] = (files, allFiles) => {
 		console.log(files.map((f) => f.meta));
 		allFiles.forEach((f) => f.remove());
 	};
 
+	const handleChangeStatus = ({ meta, file }: any, status: any) => {
+		console.log(status, meta, file);
+	};
+
 	return (
-		<Dropzone
-			getUploadParams={getUploadParams}
-			LayoutComponent={Layout}
-			onSubmit={handleSubmit}
-			classNames={classNames}
-			inputContent={InputContent}
-			PreviewComponent={PreviewContent}
-			maxFiles={10}
-			minSizeBytes={0}
-			maxSizeBytes={5000}
-			// accept={accept}
-			inputWithFilesContent={() => (
-				<span className="file-container__placeholder-browse"> Browse File</span>
-			)}
-		/>
+		<>
+			<Dropzone
+				onChangeStatus={handleChangeStatus}
+				getUploadParams={getUploadParams}
+				LayoutComponent={Layout}
+				onSubmit={handleFileSubmit}
+				classNames={classNames}
+				inputContent={(props) => (
+					<InputContent
+						{...props}
+						isOpened={isOpened}
+						closeModal={closeModal}
+						openModal={openModal}
+						getCropData={getCropData}
+						setCropper={setCropper}
+						cropData={cropData}
+						setCropData={setCropData}
+					/>
+				)}
+				PreviewComponent={PreviewContent}
+				maxFiles={10}
+				minSizeBytes={0}
+				maxSizeBytes={5000}
+				// accept={accept}
+				inputWithFilesContent={() => (
+					<span className="file-container__placeholder-browse"> Browse File</span>
+				)}
+			/>
+		</>
 	);
 };
