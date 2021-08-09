bd6cd9886d32912c8a06a4675b976e0608cac1fe
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-08-09 22:11:15 +0300

    [RT-00] fix: remove wrong logic

--- packages/react-common-services/src/service/authentication.service.ts
+++ packages/react-common-services/src/service/authentication.service.ts
@@ -19,19 +19,7 @@ export const auth = {
 	logOut: () =>
 		api.post(`${AUTH_URL}/logout`, { body: { refreshToken: storage.getRefreshToken() } }),
 
-	refreshTokenSilent: async () => {
-		try {
-			const response: LoginResponse = await auth.refreshToken({
-				refreshToken: storage.getRefreshToken(),
-			});
-			storage.persist(response.accessToken, response.refreshToken);
-			return response;
-		} catch (error) {
-			console.error('refreshTokenSilent', error);
-			storage.removeTokens();
-			throw error;
-		}
-	},
+	refreshTokenSilent: () => auth.refreshToken({ refreshToken: storage.getRefreshToken() }),
 
 	tryLoginByRefreshToken: async () => {
 		if (storage.isUserLoggedIn()) {
