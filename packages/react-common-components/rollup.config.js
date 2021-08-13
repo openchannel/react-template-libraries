2a00fb19cc136c7fbea6b4785a1d08629bfb09a7
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-08-13 14:33:20 +0300

    [RT-114] refactor: update rollup config to build with multi input files

--- packages/react-common-components/rollup.config.js
+++ packages/react-common-components/rollup.config.js
@@ -1,73 +1,59 @@
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
-import babel from '@rollup/plugin-babel';
 import external from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
 import styles from 'rollup-plugin-styles';
 import resolve from '@rollup/plugin-node-resolve';
-import url from '@rollup/plugin-url';
 import svgr from '@svgr/rollup';
 import del from 'rollup-plugin-delete';
 
 import pkg from './package.json';
 
-export default {
-	input: ['src/index.ts', 'src/chart.ts'],
-	// input: [
-	// 	'src/ui/auth/index.ts',
-	// 	'src/ui/common/index.ts',
-	// 	'src/ui/form/index.ts',
-	// 	'src/ui/management/index.ts',
-	// 	'src/ui/market/index.ts',
-	// 	'src/ui/portal/index.ts',
-	// ],
-	output: {
-		dir: 'dist',
-		format: 'esm',
-		sourcemap: true,
-		globals: {
-			react: 'React',
-			'react-dom': 'ReactDOM',
-		},
-		exports: 'named',
-		// inlineDynamicImports: true,
-	},
-	manualChunks: {
-		'chart.js': ['chart.js'],
-		'moment': ['moment'],
-		'color': ['color'],
-		'react-cropper': ['react-cropper'],
-		'react-dropzone-uploader': ['react-dropzone-uploader'],
-		'react-player': ['react-player'],
-	},
-	// manualChunks: (moduleName) => {
-	// 	// Every module whose name includes `node_modules` should be in vendor:
-	// 	if (moduleName.includes('node_modules')) {
-	// 		return 'vendor'
-	// 	}
-	// 	// Every other module will be in the chunk based on its entry point!
-	// },
-	plugins: [
-		external(),
-		// postcss({
-		// 	modules: false,
-		// 	extract: true,
-		// 	minimize: true,
-		// 	sourceMap: true
-		// }),
-		// url(),
-		svgr(),
-		styles(),
-		resolve(),
-		typescript({
-			tsconfig: 'tsconfig.build.json',
-			// rollupCommonJSResolveHack: true,
-			// clean: true,
-			// declaration: true,
-			// declarationDir: 'dist',
-		}),
-		commonjs(),
-		del({ targets: 'dist/*' }),
-	],
-	external: Object.keys(pkg.peerDependencies || {}),
+const INPUT_FILES = {
+	'index': 'src/ui/styles/styles.scss',
+	//auth
+	'auth/organisms': 'src/ui/auth/organisms/index.ts',
+	//common
+	'common/atoms': 'src/ui/common/atoms/index.ts',
+	'common/molecules': 'src/ui/common/molecules/index.ts',
+	'common/organisms': 'src/ui/common/organisms/index.ts',
+	//form
+	'form/atoms': 'src/ui/form/atoms/index.ts',
+	'form/molecules': 'src/ui/form/molecules/index.ts',
+	'form/organisms': 'src/ui/form/organisms/index.ts',
+	//management
+	'management/organisms': 'src/ui/management/organisms/index.ts',
+	//market
+	'market/atoms': 'src/ui/market/atoms/index.ts',
+	'market/molecules': 'src/ui/market/molecules/index.ts',
+	'market/organisms': 'src/ui/market/organisms/index.ts',
+	//portal
+	'portal/organisms': 'src/ui/portal/organisms/index.ts',
 };
+
+export default [
+	{
+		input: INPUT_FILES,
+		output: {
+			dir: 'dist',
+			format: 'esm',
+			sourcemap: true,
+			globals: {
+				react: 'React',
+				'react-dom': 'ReactDOM',
+			},
+			exports: 'named',
+		},
+		plugins: [
+			external(),
+			svgr(),
+			styles(),
+			resolve(),
+			typescript({
+				tsconfig: 'tsconfig.build.json',
+			}),
+			commonjs(),
+			del({ targets: 'dest/*' }),
+		],
+		external: Object.keys(pkg.peerDependencies || {}),
+	},
+];
