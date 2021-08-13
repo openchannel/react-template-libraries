0a514b374d0e51ab7fa15f111145b29223f08265
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-08-13 19:32:29 +0300

    [RT-114] refactor: format inputs, outputs for build

--- packages/react-common-components/rollup.config.js
+++ packages/react-common-components/rollup.config.js
@@ -11,23 +11,23 @@ import pkg from './package.json';
 const INPUT_FILES = {
 	'index': 'src/ui/styles/styles.scss',
 	//auth
-	'auth/organisms': 'src/ui/auth/organisms/index.ts',
+	'ui/auth/organisms/index': 'src/ui/auth/organisms/index.ts',
 	//common
-	'common/atoms': 'src/ui/common/atoms/index.ts',
-	'common/molecules': 'src/ui/common/molecules/index.ts',
-	'common/organisms': 'src/ui/common/organisms/index.ts',
+	'ui/common/atoms/index': 'src/ui/common/atoms/index.ts',
+	'ui/common/molecules/index': 'src/ui/common/molecules/index.ts',
+	'ui/common/organisms/index': 'src/ui/common/organisms/index.ts',
 	//form
-	'form/atoms': 'src/ui/form/atoms/index.ts',
-	'form/molecules': 'src/ui/form/molecules/index.ts',
-	'form/organisms': 'src/ui/form/organisms/index.ts',
+	'ui/form/atoms/index': 'src/ui/form/atoms/index.ts',
+	'ui/form/molecules/index': 'src/ui/form/molecules/index.ts',
+	'ui/form/organisms/index': 'src/ui/form/organisms/index.ts',
 	//management
-	'management/organisms': 'src/ui/management/organisms/index.ts',
+	'ui/management/organisms/index': 'src/ui/management/organisms/index.ts',
 	//market
-	'market/atoms': 'src/ui/market/atoms/index.ts',
-	'market/molecules': 'src/ui/market/molecules/index.ts',
-	'market/organisms': 'src/ui/market/organisms/index.ts',
+	'ui/market/atoms/index': 'src/ui/market/atoms/index.ts',
+	'ui/market/molecules/index': 'src/ui/market/molecules/index.ts',
+	'ui/market/organisms/index': 'src/ui/market/organisms/index.ts',
 	//portal
-	'portal/organisms': 'src/ui/portal/organisms/index.ts',
+	'ui/portal/organisms/index': 'src/ui/portal/organisms/index.ts',
 };
 
 export default [
