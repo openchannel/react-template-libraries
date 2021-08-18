74f6f67fd113e67a6bb943d4777d33cc13ec9e51
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-08-18 18:11:42 +0300

    [RT-00] refactor: remove unused code

--- packages/react-common-services/src/service/cms-site-content.service.ts
+++ packages/react-common-services/src/service/cms-site-content.service.ts
@@ -1,105 +1,32 @@
 import { forIn, has, get } from 'lodash-es';
 
-/**
- * Abstract service for working with CMS site data.<br>
- * Note (Important!): After creating service execute function {@link #initContent}<br>
- *
- * @example implementation
- *
- * import defaultCMSContent from '.../assets/content/_defaultContent.json';
- * import { CMSSiteContent, siteContent } from '@openchannel/react-common-services';
- *
- * @Injectable({
- *    providedIn: 'root',
- * })
- * export class CmsContentService extends CMSSiteContent {
- *
- *   constructor(private contentAPIService: CMSSiteContent) {
- *       super();
- *       super.initContent();
- *   }
- *
- *   getContentFromAPI(): Promise<any> {
- *      return this.contentAPIService.getContentById('siteId', 'contentId')
- *             .pipe(map(response => response.customData))
- *   }
- *
- *   getContentDefault(): any {
- *         return defaultCMSContent;
- *   }
- * }
- *
- * @example usage
- * constructor(private cmsService: CmsContentService) {...}
- *
- * this.content = this.cmsService
- *   .getContentByPaths({
- *       logoImageURL: 'default-footer.logo',
- *       contentColumns: 'default-footer.menu.items',
- *    });
- * this.cmsData = {
- *    logoImageURL: content.logoImageURL as string;
- *    contentColumns: content.contentColumns as [];
- * }
- *
- * }
- */
-export abstract class CMSSiteContent {
-	private cmsData = null;
+const _findContentByPaths = <P>(data: any, paths: P) => {
+	const tempPathsData: { [P: string]: any } = {...paths};
 
-	/**
-	 * Get content from API. Result data structure mast be equals data from {@link #getContentDefault}
-	 * When will throw an Error or Promise will be empty, service will use data from {@link #getContentDefault}
-	 * @return Promise<any> - your CMS data.
-	 */
-	abstract getContentFromAPI = () => {};
+	Object.keys(tempPathsData).forEach((key) => {
+		tempPathsData[key] = null;
+	});
 
-	/**
-	 * Get local content. Use when {@link #getContentFromAPI} did not work correctly.
-	 * @return any - your CMS data.
-	 */
-	abstract getContentDefault = () => null;
+	forIn(paths, (path, name) => {
+		tempPathsData[name] = _tryGetContentByPath(data, path);
+	});
 
-	/**
-	 * Getting content from API or local data.
-	 */
-	initContent = async () => {
-		try {
-			return await this.getContentFromAPI();
-		} catch {
-			this.cmsData = this.getContentDefault();
+	return tempPathsData;
+};
 
-			return this.cmsData;
-		}
-	};
+const _tryGetContentByPath = (cmsData: any, path: any): string | null => {
+	if (has(cmsData, path)) {
+		return get(cmsData, path);
+	} else {
+		console.warn(`CMS content. Invalid content path: ${path}`);
+		return null;
+	}
+};
 
-	/**
-	 * Returns content by paths. See 'usage' section in {@link #CMSSiteContent}
-	 */
-	getContentByPaths = <P>(paths: P): { [P: string]: any } => {
-		return this._findContentByPaths(paths);
-	};
+export const cmsSiteContent = {
+	getContentByPaths: (cmsData: any, defaultCmsData: any) => <P>(paths: P) => {
+		const data = cmsData || defaultCmsData;
 
-	private _findContentByPaths = <P>(paths: P) => {
-		const tempPathsData: { [P: string]: any } = { ...paths };
-
-		Object.keys(tempPathsData).forEach((key) => {
-			tempPathsData[key] = null;
-		});
-
-		forIn(paths, (path, name) => {
-			tempPathsData[name] = this._tryGetContentByPath(this.cmsData, path);
-		});
-
-		return tempPathsData;
-	};
-
-	private _tryGetContentByPath = (cmsData: any, path: any): string | null => {
-		if (has(cmsData, path)) {
-			return get(cmsData, path);
-		} else {
-			console.warn(`CMS content. Invalid content path: ${path}`);
-			return null;
-		}
-	};
-}
+		return _findContentByPaths(data, paths);
+	}
+};
