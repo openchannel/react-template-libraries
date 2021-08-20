4ae3015d6479df32d207c2108a4ca0d564294aee
vladimir.fedorchenko <vfedorchenko@softkit.company>
2021-08-20 12:55:42 +0300

    [RT-00] fix: FullAppData - define class instead of interface

--- packages/react-common-components/src/ui/common/models/app-data.ts
+++ packages/react-common-components/src/ui/common/models/app-data.ts
@@ -62,14 +62,20 @@ export interface CustomDataAppConfig {
 	gallery: string;
 }
 
+export interface Parent {
+	status: AppStatus;
+}
+
+/**
+ * Gallery Item interface
+ * @property {string} title - String value with image title
+ * @property {string} description - String value with description for the title
+ * @property {string} image - Image URL value
+ */
 export interface GalleryItem {
 	title: string;
 	description: string;
 	image: string;
-}
-
-export interface Parent {
-	status: AppStatus;
 }
 
 export interface App {
@@ -116,7 +122,24 @@ export interface AppVersion extends App {
 	ownership?: OwnershipModel;
 }
 
-export interface FullAppData extends AppVersion {
+/**
+ * Class used for mapping apps customData to proper fields.
+ * If you need more fields just extend this class.
+ *
+ * @example
+ * export class MyFullAppData extends FullAppData {
+ *   myField: string;
+ *
+ *   constructor(appData: AppVersion, customDataConfig: CustomDataAppConfig) {
+ *   super(appData, customDataConfig);
+ *
+ *   this.myField = appData.customData[customDataConfig.myField] || '';
+ * }
+ *
+ * const app: App = await apps.getAppById('appId')
+ * new MyFullAppData(app, {icon: 'image1', myFiled: 'fieldNameInCustomData'}))
+ */
+export class FullAppData {
 	/**
 	 * App fields
 	 */
@@ -129,8 +152,8 @@ export interface FullAppData extends AppVersion {
 	developerId: string;
 	model: AppModel[];
 	access?: string[];
-	restrict?: Record<string, unknown> | Restrict;
-	allow?: Record<string, unknown> | Restrict;
+	restrict?: {} | Restrict;
+	allow?: {} | Restrict;
 	submittedDate: number | Date;
 	created: number | Date;
 	attributes?: any;
@@ -140,6 +163,7 @@ export interface FullAppData extends AppVersion {
 	statistics: Statistics;
 	isLive: boolean;
 	type?: string;
+
 	/**
 	 * App version fields
 	 */
@@ -147,6 +171,7 @@ export interface FullAppData extends AppVersion {
 	children?: FullAppData[];
 	parent?: Parent;
 	ownership?: OwnershipModel;
+
 	/**
 	 * custom data fields mapped in constructor
 	 */
@@ -159,4 +184,50 @@ export interface FullAppData extends AppVersion {
 	categories?: string[];
 	author?: string;
 	gallery?: GalleryItem[];
+
+	/**
+	 * Create a class with mapped customData to proper fields
+	 *
+	 * @param appData app data or appversion data from api response
+	 * @param customDataConfig customDataConfig  config for customData fields name information
+	 */
+	constructor(appData: any, customDataConfig: CustomDataAppConfig) {
+		this.appId = appData.appId;
+		this.type = appData.type;
+		this.lastUpdated = new Date(appData.lastUpdated);
+		this.version = appData.version;
+		this.name = appData.name;
+		this.safeName = appData.safeName;
+		this.developerId = appData.developerId;
+		this.model = appData.model;
+		this.access = appData.access || [];
+		this.restrict = appData.restrict || null;
+		this.allow = appData.allow || null;
+		this.submittedDate = new Date(appData.submittedDate);
+		this.created = new Date(appData.created);
+		this.attributes = appData.attributes || null;
+		this.rating = appData.rating;
+		this.reviewCount = appData.reviewCount;
+		this.status = appData.status;
+		this.statistics = appData.statistics;
+		this.isLive = appData.isLive;
+		this.customData = appData.customData;
+		this.isLatestVersion = appData.isLatestVersion || null;
+		this.children = appData.children || [];
+		this.parent = appData.parent;
+		this.ownership = appData?.ownership;
+
+		// custom data mapping
+		if (customDataConfig) {
+			this.icon = appData.customData[customDataConfig.icon] || '';
+			this.logo = appData.customData[customDataConfig.logo] || '';
+			this.summary = appData.customData[customDataConfig.summary] || '';
+			this.description = appData.customData[customDataConfig.description] || '';
+			this.video = appData.customData[customDataConfig.video] || '';
+			this.images = appData.customData[customDataConfig.images] || [];
+			this.categories = appData.customData[customDataConfig.categories] || [];
+			this.author = appData.customData[customDataConfig.author] || '';
+			this.gallery = appData.customData[customDataConfig.gallery] || [];
+		}
+	}
 }
