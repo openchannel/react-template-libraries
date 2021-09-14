f2c3d117482b0411cd83dbf9f6eca7ee6175023c
vladimir.fedorchenko <onevovan@softkitit.com>
2021-09-14 16:51:20 +0300

    [RT-106] udp: service userAccountTypes

--- packages/react-common-services/src/service/user-account-types.service.ts
+++ packages/react-common-services/src/service/user-account-types.service.ts
@@ -4,26 +4,43 @@ import { UserAccountTypeModel } from '../model/api/user-type.model';
 
 const USER_ACCOUNT_TYPES_URL = 'v2/userAccountTypes';
 
-/**
- *
- * Description: Get account types list with pagination
- *
- * @param {number} pageNumber - (optional) Current page index. Starts from >= 1.
- * @param {number} limit - (optional) Count types into response. Starts from >= 1.
- * @param {string} query - (optional) Your specific search query.
- * @returns Promise<AxiosResponse<Page<UserAccountTypeModel>>>
- *
- * ### Example
- *``
- * getUserAccountTypes(1,10,"{"name": {"$in":["first", "second"]}}")
- *``
- */
 export const userAccountTypes = {
+	/**
+	 *
+	 * Description: Getting user profile fields definition by type
+	 *
+	 * @param {string} type - (required) type from the user account data
+	 * @param {any} httpOptions - (optional)
+	 * @returns {<UserAccountTypeModel>}  Promise<AxiosResponse<UserAccountTypeModel>>
+	 *
+	 * ### Example
+	 *``
+	 * getUserAccountType("developer", {"Authorization": "Bearer o8ahsd89has08dha08s7dh"})
+	 *``
+	 */
+	getUserAccountType(type: string, httpOptions?: any) {
+		return api.get<any, UserAccountTypeModel>(`${USER_ACCOUNT_TYPES_URL}/${type}`, httpOptions);
+	},
+
+	/**
+	 *
+	 * Description: Get account types list with pagination
+	 *
+	 * @param {number} pageNumber - (optional) Current page index. Starts from >= 1.
+	 * @param {number} limit - (optional) Count types into response. Starts from >= 1.
+	 * @param {string} query - (optional) Your specific search query.
+	 * @returns Promise<AxiosResponse<Page<UserAccountTypeModel>>>
+	 *
+	 * ### Example
+	 *``
+	 * getUserAccountTypes(1,10,"{"name": {"$in":["first", "second"]}}")
+	 *``
+	 */
 	getUserAccountTypes(pageNumber?: number, limit?: number, query?: string) {
 		return api.get<any, Page<UserAccountTypeModel>>(USER_ACCOUNT_TYPES_URL, {
 			params: {
-				pageNumber: pageNumber,
-				limit: limit,
+				pageNumber: String(pageNumber),
+				limit: String(limit),
 				query: query,
 			},
 		});
