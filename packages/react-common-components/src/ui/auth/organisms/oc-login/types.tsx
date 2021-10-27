c85b3723e1b6d652730e4e4e18f67f3222bae8e5
vladimir.fedorchenko <onevovan@softkitit.com>
2021-10-27 16:50:27 +0300

    [RT-163] add: new prop

--- packages/react-common-components/src/ui/auth/organisms/oc-login/types.tsx
+++ packages/react-common-components/src/ui/auth/organisms/oc-login/types.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { FormikHelpers } from 'formik';
 
 export interface LoginProps {
@@ -26,9 +27,25 @@ export interface LoginProps {
 	 */
 	onActivationLinkClick?(email: string): void;
 
+	/**
+	 * Error html template for {@link incorrectEmailErrorCode}.
+	 */
+	incorrectEmailErrorCodeTemplate?: React.ReactNode;
+
+	/**
+	 * Error html template for {@link notVerifiedEmailErrorCode}.
+	 */
+	notVerifiedEmailErrorTemplate?: React.ReactNode;
+
+	/**
+	 * Error html template for {@link passwordResetRequiredErrorCode}.
+	 */
+	passwordResetRequiredErrorTemplate?: React.ReactNode;
+
 	/**formik initial values */
 	inputEmailValue?: string;
 	inputPasswordValue?: string;
 	isIncorrectEmail?: boolean;
 	isUnverifiedEmail?: boolean;
+	isPasswordResetRequired?: boolean;
 }
